46cc33a57889cbfef2639f8c48da7bfc8423016b
Bill Sideris <bill88t@feline.gr>
2025-06-22 17:14:12 +0300

    Update DTSC

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 # Maintainer: Bill Sideris <bill88t@bredos.org>
 
 pkgname=bredos-tools
-pkgver=1.6.0
+pkgver=1.7.0
 pkgrel=1
 pkgdesc="A grand collection of tools"
 
@@ -10,10 +10,10 @@ url="https://github.com/BredOS/bredos-tools"
 license=('GPL3')
 
 groups=(bredos)
-depends=('python' 'arch-install-scripts' 'systemd')
+depends=('python' 'arch-install-scripts' 'systemd' 'gcc')
 optdepends=('dtc: Compile device trees with the dtsc helper')
 
-source=('dtsc'
+source=('dtsc.py'
         'rkdump'
         'bredos-chroot'
         'wakeupctl.py'
@@ -30,27 +30,27 @@ source=('dtsc'
         'sleepctld.1'
         'wakeupctl.1'
         'bredos-chroot.8')
-sha256sums=('SKIP'
-            'SKIP'
-            'SKIP'
-            'SKIP'
-            'SKIP'
-            'SKIP'
-            'SKIP'
-            'SKIP'
-            'SKIP'
-            'SKIP'
-            'SKIP'
-            'SKIP'
-            'SKIP'
-            'SKIP'
-            'SKIP'
-            'SKIP'
-            'SKIP')
+sha256sums=('f264899c639e3e8897e2daaef00a035a85ab51e39ba9ae5bb32d31e41d5394eb'
+            'b3a3fd7115f63180d466b05739b092912c8b62420e514f39cb36b2b345c11585'
+            '7bcf7ea1368d48876ff0991754f0be13e1fddcc0ceeef111e51dfd5461ff4451'
+            '12047c25a46a9e0def5cc687ed0d1690d8e80853680280d347b1102b5203bde3'
+            'be81b089e5bb91a9a3c2ae6c6658d538ea2b031263e3ac9685be2c1ec87fba6f'
+            'f430e73417126b2dcf84cfaa02b3fb5c520da5794faf8d29f9c8531ec970614e'
+            'ffabbfbfdca391f8616340a4323eddb868040ca35c24bd8d7d6c5df3b2cc77ac'
+            'b16aef713ffa13b9490b99a7919408671d7cbab1a109e1c5246367769711a6c8'
+            'fff6d0aa5a19bb9ab39d1696d228d2685956fbcad5cadf1817b2b2e73cb4e87f'
+            '7bde0bb9eb48c7c560194d04a0864c833e63662d3ff527a23844eaa0d1849101'
+            'ccaab9ca8f25571d5809b82f7be9a7133d91a75c745ff7174d5c78c593510659'
+            '99646c23b88b74fa6fa9220588cb7cc18b1782fa8642559ce237adfc8b98ef01'
+            '9a3d90776fb514bbcbfdb8cef0034555c093906eeac6509d3faa460fe04d7371'
+            'b0543503053367280b216f534941b39460a04461a5d19834ab679677275761c6'
+            'bb1ff999bca9352af32caca4e92cfd5516954957dd9d4397b3f4bbcde26f8302'
+            '13b871e82b556190e0f221caeb5b719b48355e908eacba1ab6ea863fb5e658e4'
+            'ef25ee68d18f85fb9a9b8a1976fcdd55828fdc968a94cdeea78490c44680f6f6')
 
 package() {
     # DTSC
-    install -Dm755 "$srcdir/dtsc" "$pkgdir/usr/bin/dtsc"
+    install -Dm755 "$srcdir/dtsc.py" "$pkgdir/usr/bin/dtsc"
 
     # RKDUMP
     install -Dm755 "$srcdir/rkdump" "$pkgdir/usr/bin/rkdump"
--- dtsc.py
+++ dtsc.py
@@ -11,7 +11,40 @@ import sys
 import argparse
 import tempfile
 import subprocess
-import shutil
+
+
+def fdt_hash_from_proc(output_file: str, mode: str = "dts") -> None:
+    """
+    Decompile live-running device tree from /proc/
+    """
+    try:
+        subprocess.check_output(
+            [
+                "dtc",
+                "-I",
+                "fs",
+                "-O",
+                mode,
+                "-@",
+                "-q",
+                "/proc/device-tree",
+                "-o",
+                output_file,
+            ],
+            stderr=subprocess.DEVNULL,
+        )
+    except subprocess.CalledProcessError:
+        pass
+
+
+def decomp_dtb(input_file: str, output_file: str) -> None:
+    try:
+        subprocess.check_output(
+            ["dtc", "-I", "dtb", input_file, "-O", "dts", "-q", "-o", output_file],
+            stderr=subprocess.DEVNULL,
+        )
+    except subprocess.CalledProcessError:
+        pass
 
 
 def preprocess_dts(input_file: str, temp_file: str, include: str, kernel: str) -> bool:
@@ -85,7 +118,16 @@ def check_dependencies() -> None:
     """
     Verify required tools are available on the system.
     """
-    if shutil.which("dtc") is None:
+
+    def is_executable_in_path(cmd: str) -> bool:
+        paths = os.environ.get("PATH", "").split(os.pathsep)
+        for path in paths:
+            full_path = os.path.join(path, cmd)
+            if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                return True
+        return False
+
+    if not is_executable_in_path("dtc"):
         print(
             "Error: The 'dtc' command is not installed or not in PATH.", file=sys.stderr
         )
@@ -94,14 +136,14 @@ def check_dependencies() -> None:
 
 def main() -> None:
     parser = argparse.ArgumentParser(
-        description="Compile a Device Tree Source (DTS) file into a Device Tree Blob (DTBO or DTB).",
-        epilog="Example: ./compile_dts.py my_device_tree.dts -o output.dtbo",
+        description="(De)Compile a Device Tree Source / Blob file.",
+        epilog="Example: dtsc my_device_tree.dts -o output.dtbo",
     )
-    parser.add_argument("input", nargs="?", help="Input DTS file")
+    parser.add_argument("input", nargs="?", help="Input file")
     parser.add_argument(
         "-o",
         "--output",
-        help="Output file (default: makes input_filename.dtb)",
+        help="Output file (Default: Follows filename)",
         default=None,
     )
     parser.add_argument(
@@ -123,37 +165,58 @@ def main() -> None:
     # Print help and exit if no arguments are provided
     if not args.input:
         parser.print_help()
-        sys.exit(1)
+        exit(1)
 
     # Verify required tools
     check_dependencies()
 
     input_file = args.input
-    output_file = args.output or input_file.rsplit(".", 1)[0] + ".dtb"
+    if input_file.lower() != "system":
+        if not input_file.endswith((".dts", ".dtb", ".dtbo")):
+            print("Input file must be .dts, .dtb, or .dtbo")
+            exit(1)
+
+    if args.output and input_file.rsplit(".", 1)[-1] == args.output.rsplit(".", 1)[-1]:
+        print("Input and output file extensions must differ.")
+        exit(1)
+
+    output_file = args.output or (
+        "system.dts"
+        if input_file.lower() == "system"
+        else input_file.rsplit(".", 1)[0]
+        + (".dtb" if input_file.endswith(".dts") else ".dts")
+    )
     include = args.include
     kernel = args.kernel
 
-    if not os.path.isfile(input_file):
+    if input_file.lower() != "system" and not os.path.isfile(input_file):
         print(f"Error: Input file '{input_file}' does not exist.", file=sys.stderr)
-        sys.exit(1)
+        exit(1)
 
-    if not (input_file.endswith(".dts") or input_file.endswith(".dtsi")):
-        print("Error: Input file must be a device tree source file.", file=sys.stderr)
-        sys.exit(1)
+    if not output_file.endswith(".dts"):
+        if input_file.lower() == "system":
+            fdt_hash_from_proc(output_file, "dtb")
+        else:
+            with tempfile.NamedTemporaryFile(delete=False) as temp:
+                temp_file = temp.name
 
-    with tempfile.NamedTemporaryFile(delete=False) as temp:
-        temp_file = temp.name
+            try:
+                if not preprocess_dts(input_file, temp_file, include, kernel):
+                    exit(1)
+                if not compile_dts(temp_file, output_file, include):
+                    exit(1)
+            finally:
+                if os.path.exists(temp_file):
+                    os.remove(temp_file)
 
-    try:
-        if not preprocess_dts(input_file, temp_file, include, kernel):
-            sys.exit(1)
-        if not compile_dts(temp_file, output_file, include):
-            sys.exit(1)
-    finally:
-        if os.path.exists(temp_file):
-            os.remove(temp_file)
-
-    print(f"DTBO successfully created: {output_file}")
+            print(
+                f"Device Tree Blob {'Overlay ' if output_file.endswith('.dtbo') else ''}successfully created: {output_file}"
+            )
+    else:
+        if input_file.lower() == "system":
+            fdt_hash_from_proc(output_file)
+        else:
+            decomp_dtb(input_file, output_file)
 
 
 if __name__ == "__main__":
